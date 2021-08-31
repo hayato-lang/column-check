27c2eb2d3a82c22668a1dcc70c2cc1aaa59667f4
FjiiNamiki <archerynamiki@gmail.com>
2021-08-31 14:46:58 +0900

    テストコードとルボコップの実行

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                  {'エマ'}
+    email                 {Faker::Internet.free_email}
+    password              {'abc123'}
+    password_confirmation {password}
+  end
+end
