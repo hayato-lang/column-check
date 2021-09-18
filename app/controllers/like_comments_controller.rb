03f54e3de4c038013aee09c6d2453a4d18dc9cea
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-09-18 15:04:03 +0900

    コントローラーにアクションを追加

--- app/controllers/like_comments_controller.rb
+++ app/controllers/like_comments_controller.rb
@@ -1,2 +1,11 @@
 class LikeCommentsController < ApplicationController
+  def create
+    @comment = Comment.find(params[:comment])
+    current_user.like(@comment)
+  end
+
+  def destroy
+    @comment = Like.find(params[:id]).comment
+    current_user.unlike(@comment)
+  end
 end
