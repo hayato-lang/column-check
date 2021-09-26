cd9d7e44d1c75aed298b8172b6a42dd4de12d3d9
FjiiNamiki <archerynamiki@gmail.com>
2021-09-26 11:08:29 +0900

    updateアクションとdestroyアクションを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update,:destroy]
+  before_action :set_post_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.includes(:user).order('created_at DESC')
@@ -29,7 +30,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    
+    if @post.update(post_params)
+      redirect_to action: :show
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
@@ -39,5 +49,10 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+  def set_post_user
+    if @post.user != current_user
+      redirect_to root_path
+    end
   end
 end
